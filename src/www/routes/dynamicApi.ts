ce0bf1430b79223d9dd49102650cc271f0e1dc07
Quinn Lane <hello@quinnlane.dev>
2022-02-28 19:13:36 -0500

    :bug: Fixed dynamic API not checking for valid IPs I forgot to include checks on some of the endpoints to validate whether the input was a valid IP or not
    Signed-off-by: Quinn Lane <hello@quinnlane.dev>

--- src/www/routes/dynamicApi.ts
+++ src/www/routes/dynamicApi.ts
@@ -50,22 +50,34 @@ router.get('/:ip/location', (req, res) => {
 // Gets the timezone and current date and time for a location derived from an IP.
 router.get('/:ip/timezone', (req, res) => {
   let g = geoip.lookup(req.params.ip)
-  res.setHeader('content-type', 'text/json')
-  res.send({
-    timezone: g?.timezone,
-    currentTime: new Date().toLocaleString('en-US', { timeZone: g?.timezone })
-  })
+  if (!isIP(req.params.ip)) {
+    res.status(400)
+    res.setHeader('content-type', 'text/plain')
+    res.send('The IP provided is not valid! Please check the IP and try again')
+  } else {
+    res.setHeader('content-type', 'text/json')
+    res.send({
+      timezone: g?.timezone,
+      currentTime: new Date().toLocaleString('en-US', { timeZone: g?.timezone })
+    })
+  }
 })
 
 // Version Response
 // Gets the current version of the IP (IPv4 or IPv6)
 router.get('/:ip/version', (req, res) => {
   let i = req.params.ip
-  res.setHeader('content-type', 'text/json')
-  res.send({
-    version: ipVersion(i),
-    humanReadable: `IPv${ipVersion(i)}`
-  })
+  if (!isIP(i)) {
+    res.status(400)
+    res.setHeader('content-type', 'text/plain')
+    res.send('The IP provided is not valid! Please check the IP and try again')
+  } else {
+    res.setHeader('content-type', 'text/json')
+    res.send({
+      version: ipVersion(i),
+      humanReadable: `IPv${ipVersion(i)}`
+    })
+  }
 })
 
 export default router
