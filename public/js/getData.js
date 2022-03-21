d2ab876cd5e84c8ad0ecd8cbd689ed7792741317
Quinn Lane <hello@quinnlane.dev>
2022-03-21 00:14:08 -0400

    :lipstick: Jazz up the frontend UI
    Signed-off-by: Quinn Lane <hello@quinnlane.dev>

--- public/js/getData.js
+++ public/js/getData.js
@@ -1,13 +1,17 @@
 (async () => {
-  // Get IP
-  const ipResponse = await fetch('/api/just-ip', {
-    method: 'get'
-  })
-  document.getElementById('ip').innerHTML = await ipResponse.text()
+  try {
+    // Get IP
+    const ipResponse = await fetch('/api/just-ip', {
+      method: 'get'
+    })
+    document.getElementById('ip').innerHTML = await ipResponse.text()
 
-  // Get location data
-  const locResponse = await fetch('/api/location', {
-    method: 'get'
-  })
-  document.getElementById('loc').innerHTML = JSON.parse(await locResponse.text()).municipality.humanReadable
+    // Get location data
+    const locResponse = await fetch('/api/location', {
+      method: 'get'
+    })
+    document.getElementById('loc').innerHTML = JSON.parse(await locResponse.text()).municipality.humanReadable
+  } catch (e) {
+    document.getElementById("ip_str").innerHTML = "<strong>Error:</strong> For whatever reason, we could not make connection to API!"
+  }
 })();
